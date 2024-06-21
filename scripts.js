d11792a04d31e58df4ba40b3617f05728bea1c3a
WilliamRG2004 <willrg2323@gmail.com>
2024-06-21 17:52:06 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+function showhide(){
+    var conteiner=document.getElementsByClassName("conteiner")[0];
+    if(conteiner.style.visibility=="hidden"){
+        conteiner.style.visibility="visible";
+
+    }else{
+        conteiner.style.visibility="hidden";
+
+    }
+
+}
+
+
+function privaci(){
+    var priva=document.getElementsByClassName("priva")[0];
+    if(priva.style.visibility=="hidden"){
+        priva.style.visibility="visible";
+
+    }else{
+        priva.style.visibility="hidden";
+
+    }
+
+}
+
+
+function pago(){
+    var pagos=document.getElementsByClassName("pagos")[0];
+    if(pagos.style.visibility=="hidden"){
+        pagos.style.visibility="visible";
+
+    }else{
+        pagos.style.visibility="hidden";
+
+    }
+
+}
+
+
+
+function respi(){
+    var respira=document.getElementsByClassName("respira")[0];
+    if(respira.style.visibility=="hidden"){
+        respira.style.visibility="visible";
+
+    }else{
+        respira.style.visibility="hidden";
+
+    }
+
+}
